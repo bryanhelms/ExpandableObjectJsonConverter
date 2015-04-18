41842c370a24dc6f9a309966546860a862364e43
Bryan Helms <bryan@bryanhelms.com>
2015-04-17 22:53:10 -0700

    Make unit tests that demonstrate and verify usage

---
+++ test/ExpandableObjectJsonConverter.Tests/ExpandableObjectJsonConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace ExpandableObjectJsonConverter.Tests
+{
+    public class ExpandableObjectJsonConverterTests
+    {
+        [Fact]
+        public void Object_id_only_deserializes_with_id_and_not_expanded()
+        {
+            var json = JsonConvert.SerializeObject(new {
+                firstvalue = "firsttestvalue",
+                example = "exampleobjectid"
+            });
+
+            var actual = JsonConvert.DeserializeObject<TopLevelObject>(json);
+
+            Assert.Equal("firsttestvalue", actual.FirstValue);
+            Assert.NotNull(actual.Example);
+            Assert.Equal("exampleobjectid", actual.Example.Id);
+            Assert.False(actual.Example.IsExpanded);
+        }
+
+        [Fact]
+        public void Expanded_object_deserializes_with_full_object()
+        {
+            var json = JsonConvert.SerializeObject(new {
+                firstValue = "firsttestvalue",
+                example = new {
+                    id = "otherrandomid",
+                    someValue = "somerandomvalue",
+                    otherValue = 42
+                }
+            });
+
+            var actual = JsonConvert.DeserializeObject<TopLevelObject>(json);
+
+            Assert.Equal("firsttestvalue", actual.FirstValue);
+            Assert.NotNull(actual.Example);
+            Assert.True(actual.Example.IsExpanded);
+            Assert.Equal("otherrandomid", actual.Example.Id);
+            Assert.Equal("somerandomvalue", actual.Example.SomeValue);
+            Assert.Equal(42, actual.Example.OtherValue);
+        }
+    }
+}
